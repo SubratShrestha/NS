85a50d5d63d857269c3386ff9311eccecb7680bd
olsenmatthew <olsenmatthew780@gmail.com>
2020-05-16 13:02:58 +1000

    added device page with tabs

--- NeuroStim.py
+++ NeuroStim.py
@@ -1,6 +1,25 @@
 import kivy
 from kivy.app import App
+from kivy.lang import Builder
 from kivy.uix.floatlayout import FloatLayout
+from kivy.uix.screenmanager import ScreenManager, Screen
+from kivy.uix.tabbedpanel import TabbedPanel
+
+
+class HomeScreen(Screen):
+    pass
+
+class DeviceScreen(Screen):
+    pass
+
+class ScreenManagement(ScreenManager):
+    pass
+
+class DeviceTabs(TabbedPanel):
+    pass
+
+kv_loader = Builder.load_file("neurostim.kv")
+
 
 class HomeLayout(FloatLayout):
     def __init__(self):
@@ -8,8 +27,7 @@ class HomeLayout(FloatLayout):
 
 class NeuroStimApp(App):
     def build(self):
-        return HomeLayout()
-
+        return kv_loader
 
 if __name__ == "__main__":
     NeuroStimApp().run()
