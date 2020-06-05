29a47aee4869da975a1a729c9dd8d50c0c7afb5d
MitchelZha <64114381+MitchelZha@users.noreply.github.com>
2020-06-05 16:56:17 +1000

    Add files via upload

--- graph generator.py
+++ graph generator.py
@@ -6,7 +6,7 @@ burst = int (burst)
 if burst == 1:
     bursttime = input("bursttime (ms): \n")
     bursttime = int(bursttime)*1000
-    interburst = input("interburst (s): (type 0 to use frequency) \n")
+    interburst = input("interburst (ms): (type 0 to use frequency) \n")
     interburst = int(interburst)*1000*1000
     if interburst == 0:
         burstfrequency = input("burstfrequency (Hz): \n")
@@ -25,15 +25,14 @@ interphase = input("interphase (us): \n")
 interphase = int (interphase)
 interstim = input("interstim (us): (type 0 to use frequency) \n")
 interstim = int (interstim)
-#if interstim == 0:
-    #wavefrequency = input("wavefrequency (pps): \n")
-    #wavefrequency = int (wavefrequency)
-    #interstim = 1000 / wavefrequency - phasetime1 - phasetime2 - interphase
-    #Unit need to be fixed
+if interstim == 0:
+    wavefrequency = input("wavefrequency (p.p.s): \n")
+    wavefrequency = int (wavefrequency)
+    interstim = 1000000 / wavefrequency - phasetime1 - phasetime2 - interphase
 
 #points on y-axis
-andoic = [0, 1, 1, 0, 0, -1, -1, 0]
-cathodic = [0, -1, -1, 0, 0, 1, 1, 0]
+andoic = [0, 1, 1, 0, 0, -1, -1, 0, 0]
+cathodic = [0, -1, -1, 0, 0, 1, 1, 0, 0]
 if anodic == 1:
     V = [i * current * 0.001 for i in andoic]
 else:
@@ -47,7 +46,7 @@ T = [0, 0, phasetime1, phasetime1, phasetime1+interphase, phasetime1+interphase,
 # if its burst stim, graph stop plotting after one burst peroid
 if burst == 1:
     # constantly add last element of previous "T list"to all element in previous T to make a new list of timing for next peroid. and then join all lists together to form a set for y-axis data
-    T = b.copy()
+    b = T.copy()
     while (b[len(b)-1]<(bursttime + interburst)):
         for i in range(len(b)):
             b[i]=T[i]+b[len(b)-1]
@@ -55,15 +54,20 @@ if burst == 1:
 
     #change the T[-1]to bursttime
     T[len(T)-1] = bursttime
-    #add one more element to T, that is bursttime + interburst
-    T.append(burstime + interburst)
-    #count how many element in the list, divide by the 8, and repeat V this many time, make set V2
-    V * (len(T) / 8)
+    #count how many element in the T list, divide by the 9, and repeat V list this many time, make set V2
+    #V * (len(T) / 9)
+    m = V.copy()
+    for i in range(int(len(T)/9.0-1)):
+        m = m + V
+    V = m
+    # add one more element to T, that is bursttime + interburst
+    T.append(bursttime + interburst)
+    V.append(0)
 
-#print (V)
-#print (T)
+#print(len(V))
+#print(len(T))
 
-plt.plot(V, T)
-plt.xlabel('voltage (mV)')
-plt.ylabel('time (us)')
+plt.plot(T,V)
+plt.xlabel('time (us)')
+plt.ylabel('voltage (mV)')
 plt.show()
