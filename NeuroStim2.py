ab4c86612b84198cf13bcc181757d59874f03124
olsenmatthew <olsenmatthew780@gmail.com>
2020-06-07 11:10:00 +1000

    added graph library

--- NeuroStim2.py
+++ NeuroStim2.py
@@ -26,6 +26,7 @@ from kivy.config import Config
 Config.set('graphics', 'width', 1024)
 Config.set('graphics', 'height', 768)
 Config.set('graphics', 'resizable', 'False')
+"""Must be below config setting, otherwise it's reset"""
 from kivy.garden.matplotlib.backend_kivyagg import FigureCanvasKivyAgg
 
 
