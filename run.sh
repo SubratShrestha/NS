df8887e6ac7a45ec163e4ef80070cf932690bb32
olsenmatthew <olsenmatthew780@gmail.com>
2020-07-23 12:10:27 +1000

    ubuntu update

--- run.sh
+++ run.sh
@@ -3,22 +3,20 @@
 echo "Running Application for $OSTYPE"
 echo $1
 if [[ "$OSTYPE" == "linux-gnu"* ]]; then
-python App.py $1 &
-echo "Starting on Windows";
+python Windows.py &
+sleep 1;
+python ble/ble.py $1 &
 elif [[ "$OSTYPE" == "darwin"* ]]; then
 python App.py $1 &
 elif [[ "$OSTYPE" == "cygwin" ]]; then
-        # POSIX compatibility layer and Linux environment emulation for Windows
 python Windows.py &
 sleep 1;
 python ble/ble.py $1 &
 elif [[ "$OSTYPE" == "msys" ]]; then
-# Lightweight shell and GNU utilities compiled for Windows (part of MinGW)
 python Windows.py &
 sleep 1;
 python ble/ble.py $1 &
 elif [[ "$OSTYPE" == "win32" ]]; then
-# I'm not sure this can happen.
 python Windows.py &
 sleep 1;
 python ble/ble.py $1 &
